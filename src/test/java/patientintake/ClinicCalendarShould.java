92455763bdf9d96f9e2c67ef2e8466001632e85c
Luiz Gustavo do Amaral Correa <l.do.amaral.correa@avanade.com>
2022-03-21 15:49:00 -0300

    feat: Adicionando comentários. Incluindo testes de registro a registro

--- src/test/java/patientintake/ClinicCalendarShould.java
+++ src/test/java/patientintake/ClinicCalendarShould.java
@@ -3,6 +3,7 @@ package patientintake;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.junit.Test;
@@ -22,8 +23,23 @@ public class ClinicCalendarShould {
 		
 		List<PatientAppointment> appointments = calendar.getAppointments();
 		
+		//vou testar se a lista é vazia
 		assertNotNull(appointments);
+		
+		//teste se está sendo incluído apenas 1 registro na lista
+		// primeiro parametro é o valor esperado
+		// segundo é o valor de retorno
 		assertEquals(1, appointments.size());
+		
+		//farei a verificação de campo a campo
+		PatientAppointment enteredAppt = appointments.get(0);
+		assertEquals("Jim", enteredAppt.getPatientFirstName());
+		assertEquals("Weaver", enteredAppt.getPatientLastName());
+		assertEquals(Doctor.avery, enteredAppt.getDoctor());
+		//usamos a formatação de data no valor que recebemos.
+		assertEquals("9/1/2022 10:00 PM", 
+				enteredAppt.getAppointmentDateTime().format(DateTimeFormatter.ofPattern("M/d/yyyy hh:mm a")));
+		
 	}
 
 }
